b472ed766d8b08293e55af0db3a1e0b6bdd9ef8d
Piyush Tiwari <kashyappiyush1998@gmail.com>
2024-12-16 02:50:14 +0530

    intitate github and check if browser supports WebGPU

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+const output_label : HTMLElement = <HTMLElement> document.getElementById("compatibility-check");
+
+if (navigator.gpu) {
+    output_label.innerText = "This browser supports WebGPU";
+}
+else {
+    output_label.innerText = "This browser does not supports WebGPU";
+}
