a22474cd5a42100cd547e681e5f63bc12bd524cb
Piyush Tiwari <kashyappiyush1998@gmail.com>
2024-12-16 20:21:36 +0530

    remove dev dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,72 @@
-const output_label : HTMLElement = <HTMLElement> document.getElementById("compatibility-check");
+import shader from "./shaders/shaders.wgsl"
 
-if (navigator.gpu) {
-    output_label.innerText = "This browser supports WebGPU";
+const Initialize = async () => {
+
+    const canvas : HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("gfx-main");
+    const adapter : GPUAdapter = <GPUAdapter> await navigator.gpu?.requestAdapter();
+    const device : GPUDevice = <GPUDevice> await adapter?.requestDevice();
+    const context : GPUCanvasContext = <GPUCanvasContext> canvas.getContext("webgpu");
+    const format : GPUTextureFormat = "bgra8unorm";
+    context.configure({
+        device: device,
+        format: format,
+        alphaMode: "opaque",
+    });
+
+    const bindGroupLayout = device.createBindGroupLayout({
+        entries: [],
+    });
+
+    const bindGroup = device.createBindGroup({
+        layout: bindGroupLayout,
+        entries: []
+    });
+
+    const pipelineLayout = device.createPipelineLayout({
+        bindGroupLayouts: [bindGroupLayout]
+    });
+
+    const pipeline : GPURenderPipeline = device.createRenderPipeline({
+        vertex : {
+            module : device.createShaderModule({
+                code: shader
+            }),
+            entryPoint : "vs_main" 
+        },
+
+        fragment : {
+            module : device.createShaderModule({
+                code: shader
+            }),
+            entryPoint : "fs_main",
+            targets : [{
+                format : format
+            }]
+        },
+
+        primitive : {
+            topology : "triangle-list"
+        }, 
+        layout : pipelineLayout
+    });
+
+    const commandEncoder : GPUCommandEncoder = device.createCommandEncoder();
+    const textureView : GPUTextureView = context.getCurrentTexture().createView();
+    const renderpass : GPURenderPassEncoder = commandEncoder.beginRenderPass({
+        colorAttachments : [{
+            view: textureView,
+            clearValue: {r: 0.5, g: 0.0, b: 0.25, a: 1.0},
+            loadOp: "clear",
+            storeOp: "store"
+        }]
+    });
+    renderpass.setPipeline(pipeline);
+    renderpass.setBindGroup(0, bindGroup)
+    renderpass.draw(3, 1, 0, 0);
+    renderpass.end();
+
+    device.queue.submit([commandEncoder.finish()]);
+
 }
-else {
-    output_label.innerText = "This browser does not supports WebGPU";
-}
+
+Initialize();
